efadafcae1db9d776b4e9156b7d162e202ba72f0
pankaj1131 <pankaj_singh1131@yahoo.com>
2019-10-14 18:32:29 +0100

    Add files via upload
    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+
+movie_character_metadata_filename = './data/movie_characters_metadata.txt'
+movie_conversations_filename = './data/movie_conversations.txt'
+movie_lines_filename = './data/movie_lines.txt'
+movie_titles_metadata_filename = './data/movie_titles_metadata.txt'
